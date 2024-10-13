c77ba385bec676df87f1c340e1f4acdf5eca7fc9
aytan97 <aytan.sh.aa@gmail.com>
2024-10-14 03:41:30 +0400

    Test

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const { google } = require('googleapis')
 const multer = require('multer')
 const cors = require('cors')
 const stream = require('stream')
-const googleConfigPath = require('./key.js')
 require('dotenv').config()
 // Initialize Express app
 const authenticate = require('./auth')
@@ -37,7 +36,20 @@ app.post('/upload', authenticate, upload.array('files'), async (req, res) => {
   try {
     // Google Drive authentication setup
     const auth = new google.auth.GoogleAuth({
-      keyFile: googleConfigPath,
+      credentials: {
+        type: process.env.GOOGLE_TYPE,
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+        token_uri: 'https://oauth2.googleapis.com/token',
+        auth_provider_x509_cert_url:
+          'https://www.googleapis.com/oauth2/v1/certs',
+        client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+        universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+      },
       scopes: ['https://www.googleapis.com/auth/drive'],
     })
 
