dbc170f46b0a7638f441fc376908bb1834349ab0
aytan97 <aytan.sh.aa@gmail.com>
2024-10-14 02:36:27 +0400

    Simple Google Drive file upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Ensure you import path properly in ESM style
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,8 @@ export default defineConfig({
   server: {
     port: 5175,
     host: true,
+  },
+  build: {
+    outDir: 'dist', // Ensure this is set to 'dist'
   },
 })
