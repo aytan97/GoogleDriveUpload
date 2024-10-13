db238a1486ef2a98c8b8a021bdc0200dc6ec1b9e
aytan97 <aytan.sh.aa@gmail.com>
2024-10-14 03:43:11 +0400

    Test

--- server/key.js
+++ server/key.js
@@ -1,23 +1,23 @@
-const fs = require('fs')
-require('dotenv').config()
+// const fs = require('fs')
+// require('dotenv').config()
 
-const googleConfig = {
-  type: process.env.GOOGLE_TYPE,
-  project_id: process.env.GOOGLE_PROJECT_ID,
-  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY,
-  client_email: process.env.GOOGLE_CLIENT_EMAIL,
-  client_id: process.env.GOOGLE_CLIENT_ID,
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
-}
+// const googleConfig = {
+//   type: process.env.GOOGLE_TYPE,
+//   project_id: process.env.GOOGLE_PROJECT_ID,
+//   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+//   private_key: process.env.GOOGLE_PRIVATE_KEY,
+//   client_email: process.env.GOOGLE_CLIENT_EMAIL,
+//   client_id: process.env.GOOGLE_CLIENT_ID,
+//   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+//   token_uri: 'https://oauth2.googleapis.com/token',
+//   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+//   client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+//   universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+// }
 
-// Save to a JSON file (optional)
-const configFilePath = 'googleConfig.json'
-fs.writeFileSync(configFilePath, JSON.stringify(googleConfig, null, 2))
+// // Save to a JSON file (optional)
+// const configFilePath = 'googleConfig.json'
+// fs.writeFileSync(configFilePath, JSON.stringify(googleConfig, null, 2))
 
-// Export the file path
-module.exports = configFilePath
+// // Export the file path
+// module.exports = configFilePath
